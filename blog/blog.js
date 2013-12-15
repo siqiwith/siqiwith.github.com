b4902be80dbf4cd43f966e7f9e5e2955482b2c38
Siqi <siqi.zhong@gmail.com>
2013-12-15 22:46:28 +0800

    Add files for blog.

---
+++ blog/blog.js
@@ -0,0 +1,33 @@
+﻿/**
+ * Copyright © 2013 Siqi. All Rights Reserved.
+ * Licensed under the GPL license.
+ */
+$(function(){
+	var started = false;
+	var length = 2000;
+	var cloudClasses = ["small-cloud", "medium-cloud", "big-cloud"];
+	function createCloud(){
+		var randomDuration = 70000 + 30000 * Math.random(); 
+		var randomInterval = started ? 40000 * Math.random() : 0;
+		var cloudType = Math.floor(3 * Math.random());
+		var height = $(window).height() > 210 ? $(window).height() - 210 : 0;
+		var top = Math.random() * height;
+		var zIndex = -999;
+		setTimeout(function(){
+			$("<div></div>", {
+				"class": cloudClasses[cloudType],
+				"css": {
+					"top": top + "px",
+					"z-index": zIndex
+				}
+			}).appendTo('body').animate({
+				left: '+=' + length
+			}, randomDuration, 'linear', function() {
+				$(this).remove();
+			});
+			createCloud();
+		}, randomInterval);
+	};
+	createCloud();
+	started = true;
+});
